08773cbf7137e7b5e201bc017086f3e02f41cbcf
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-06-11 23:13:04 -0500

    Moving Zeros work

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,8 +4,33 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
+    for i in range(len(arr)):
+        if arr[i] == 0:
+            x = 0
+            end = len(arr) - 1
+            lastvalue = arr[end]
+            arr[end] = x
+            while lastvalue == 0 and end is not i:
+                end -= 1
+                temp = arr[end]
+                arr[end] = lastvalue
+                lastvalue = temp
 
-    pass
+            arr[i] = lastvalue
+
+            # if lastvalue == 0:
+            #     z = arr[len(arr)-1]
+            #     z =
+            #     arr[len(arr)-1] = lastvalue
+            #
+            # arr[i] = lastvalue
+    return arr
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
